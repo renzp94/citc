2b21bd3ce8a7a1ed63f8c218d56afb6c5e3c6497
renzp94 <1074720760@qq.com>
2022-03-07 14:57:59 +0800

    fix(scripts): 修复favicon配置问题

--- packages/scripts/src/configs/common/plugins/html-webpack-plugin.ts
+++ packages/scripts/src/configs/common/plugins/html-webpack-plugin.ts
@@ -1,15 +1,23 @@
 import type WebpackChain from 'webpack-chain'
 import type { Options } from 'html-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { fileExists, pathResolve } from '../../../utils'
 
 export default (
   webpackChain: WebpackChain,
   { fileType, html }: { fileType: string; html: Options }
 ) => {
-  const { title, template = 'index.html', favicon = 'favicon.ico' } = html ?? {}
-  webpackChain
-    .plugin('html-webpack-plugin')
-    .use(HtmlWebpackPlugin, [
-      { title: title ?? `Citc ${fileType} Demo`, template, favicon, ...html },
-    ])
+  const { title, template = 'index.html' } = html ?? {}
+  let defaultFavicon: false | string = 'favicon.ico'
+  const defaultFaviconExist = fileExists(pathResolve(process.cwd(), defaultFavicon))
+  defaultFavicon = defaultFaviconExist ? 'favicon.ico' : false
+
+  webpackChain.plugin('html-webpack-plugin').use(HtmlWebpackPlugin, [
+    {
+      title: title ?? `Citc ${fileType} Demo`,
+      template,
+      favicon: html?.favicon ?? defaultFavicon,
+      ...html,
+    },
+  ])
 }
